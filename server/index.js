67d715f4165ae421009576f0adecdf47c6f3548f
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-08-13 16:47:53 +0500

    socket error debugging

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const http = require('http');
 const express = require('express');
 const cors = require('cors');
 const socketIO = require("socket.io");
+const { socket } = require('dgram');
 
 const app = express();
 const port = 4500 || process.env.PORT;
+const users = [{}];
 
 app.use(cors());
 app.get("/", (req, res) => {
@@ -15,13 +17,22 @@ app.get("/", (req, res) => {
 const server = http.createServer(app);
 const io = socketIO(server);
 
-// io.on("connection", ()=> {
-//     console.log("New Connection");
-// })
-io.on("connection", () => {
+io.on("connection", (socket) => {
     console.log("New Connection");
-})
 
-server.listen(port, ()=> {
+    
+
+    socket.on('joined', ({ user }) => {
+        users[socket.id] = user;
+        console.log(`${user} has joined`);
+
+    })
+
+    socket.emit('Welcome', {user: "Admin", message: "Welcome to chatiFy"});
+    
+
+
+});
+server.listen(port, () => {
     console.log(`Server is working on http://localhost:${port}`);
 })
