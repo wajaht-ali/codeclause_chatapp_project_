ff5015869f9bbd2ba9067590b5008194b063fae0
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-08-17 11:25:40 +0500

    join updates has been done

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const http = require('http');
 const express = require('express');
 const cors = require('cors');
 const socketIO = require("socket.io");
-const { socket } = require('dgram');
+const socket = require('dgram');
+// const user = require("../chatify/src/components/join/join");
 
 const app = express();
 const port = 4500 || process.env.PORT;
@@ -20,17 +21,19 @@ const io = socketIO(server);
 io.on("connection", (socket) => {
     console.log("New Connection");
 
-    
+
 
     socket.on('joined', ({ user }) => {
         users[socket.id] = user;
         console.log(`${user} has joined`);
-
+        socket.broadcast.emit('userJoined', { user: "Admin", message: `${users[socket.id]} has joined` });
+        socket.emit('Welcome', { user: "Admin", message: "Welcome to chatiFy" });
     })
 
-    socket.emit('Welcome', {user: "Admin", message: "Welcome to chatiFy"});
-    
-
+    socket.on('offline', () => {
+        socket.broadcast.emit("leave", {user: "Admin", message: "user has left"} );
+        console.log("User left");
+    })
 
 });
 server.listen(port, () => {
